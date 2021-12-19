ddd97995ee4a8d14625fa67252fa4c0bd8ac0374
root <izainula@gmail.com>
2021-12-19 03:05:05 -0800

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/irfan')
+def hello():
+    return 'Hi This is IRFAN.I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
